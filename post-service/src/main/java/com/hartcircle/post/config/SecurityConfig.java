8ca10c798cb543944181127b71fcffbc0d850dcd
thisaru-sachintha <asthisaru@gmail.com>
2025-10-03 18:26:04 +0530

    post-service and bid-service security config cors error fix

--- post-service/src/main/java/com/hartcircle/post/config/SecurityConfig.java
+++ post-service/src/main/java/com/hartcircle/post/config/SecurityConfig.java
@@ -1,13 +1,15 @@
 package com.hartcircle.post.config;
 
-import com.hartcircle.post.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.hartcircle.post.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,17 +22,19 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable()) 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/api/v1/user/CreatePost",
-                                "/api/v1/post/image/**"
+                                "/api/v1/post/image/**",
+                                "/api/v1/post/publicWall",
+                                "/api/v1/post/**",
+                                "/api/v1/post/summary/getPostview",
+                                "/api/v1/post/getPostData/**",
+                                "/api/v1/user/summary/**",
+                                "/api/v1/post/ratethisPost/**"
                         ).authenticated()
-                        .requestMatchers("/api/v1/post/publicWall").authenticated()
-                        .requestMatchers("/api/v1/post/**").authenticated()
-                        .requestMatchers("/api/v1/post/summary/getPostview").authenticated()
-                        .requestMatchers("/api/v1/post/getPostData/**").authenticated()
-                        .requestMatchers("/api/v1/user/summary/**").authenticated()
-                        .requestMatchers("/api/v1/post/ratethisPost/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
